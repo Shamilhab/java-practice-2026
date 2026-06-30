aec72c39af505a0313d93cc57989ec9283037bcd
Shamil Habibulin <shahabibulin@gmail.com>
2026-06-30 03:21:58 +0300

    second  commit

---
+++ src/main/java/ru/itis/shop/user/infrastucture/persistence/UserFileRepository.java
@@ -0,0 +1,48 @@
+package ru.itis.shop.user.infrastucture.persistence;
+
+import ru.itis.shop.user.domain.User;
+import ru.itis.shop.user.repository.UserRepository;
+
+import java.io.*;
+import java.util.UUID;
+
+public class UserFileRepository implements UserRepository {
+
+    private final String fileName;
+
+    public UserFileRepository(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void save(User user) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))) {
+            String id = UUID.randomUUID().toString();
+            user.setId(id);
+            writer.write(user.getId() + "|" +
+                    user.getEmail() + "|" +
+                    user.getPassword() + "|" +
+                    user.getProfileDescription());
+            writer.newLine();
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public User findById(String id) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            String[] yourData = new String[4];
+
+            while ((line = reader.readLine()) != null) {
+                yourData = line.split("\\|");
+                if (yourData[0].equals(id)) {
+                    return new User(yourData[0], yourData[1], yourData[2], yourData[3]);
+                }
+            }
+
+        } catch (IOException e) { e.printStackTrace(); }
+        return null;
+    }
+}
